c0e3316b30082586459ed97512abb8c8b9032d3b
Marya Zvereva <mashechka28@mail.ru>
2024-05-13 21:33:43 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/SelenideTest.java
@@ -0,0 +1,35 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class SelenideTest {
+
+    @BeforeAll
+    public static void setupAll() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        WebDriverManager.chromedriver().capabilities(options).setup();
+    }
+
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$(".input__control").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".paragraph").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
